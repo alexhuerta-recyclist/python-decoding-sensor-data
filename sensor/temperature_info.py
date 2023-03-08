662bcb6ba91ce69014e4a227189c83502ac3b39b
alexhuerta-recyclist <alex@recyclist.co>
2023-03-08 14:16:27 -0500

    Update temperature_info.py

--- sensor/temperature_info.py
+++ sensor/temperature_info.py
@@ -4,8 +4,7 @@ class TemperatureData(HouseInfo):
     def _convert_data(self, data):
         recs = []
         for rec in data:
-            rec = int(rec, base=10)
-            recs.append(rec)
+            recs.append(int(rec, base=10))
         return recs
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("temperature", rec_area)
